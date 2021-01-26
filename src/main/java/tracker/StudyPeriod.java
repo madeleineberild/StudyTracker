bd6d61e8a3839e01c00e9ffbd1cc51ed38a210b6
madeleineberild <madeleine.berild@hotmail.com>
2021-01-26 17:07:31 +0100

    Add abstract class StudyRecorder

--- src/main/java/tracker/StudyPeriod.java
+++ src/main/java/tracker/StudyPeriod.java
@@ -6,32 +6,16 @@ import java.util.List;
 /**
  * Created for each semester/term and contains a number of StudyEntries
  */
-public class StudyPeriod {
-    String name;
+public class StudyPeriod extends StudyRecorder {
     int weeks;
-    List<StudyEntry> recorded;
 
     public StudyPeriod(String name, int weeks) {
-        this.name = name;
+        super(name);
         this.weeks = weeks;
-        this.recorded = new ArrayList<>();
-    }
-
-    public String getName() {
-        return name;
     }
 
     public int getWeeks() {
         return weeks;
-    }
-
-    public void record(StudyEntry e) {
-        recorded.add(e);
-    }
-
-    public void getRecorded() {
-        //TODO: hitta ett sätt där vi kan ge ut en struktur med alla entries
-        // (till statistics) som inte är mutable och kan fucka upp allt
     }
 
 }
