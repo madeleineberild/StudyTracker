f5194f825e72c864ed6c1873b3130dd424e8b76a
madeleineberild <madeleine.berild@hotmail.com>
2021-01-26 17:21:42 +0100

    Implement FileWriter functions

--- src/main/java/util/FileWriter.java
+++ src/main/java/util/FileWriter.java
@@ -1,4 +1,15 @@
 package util;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class FileWriter {
+
+    public void writeToFile(String outputString) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new java.io.FileWriter("history.txt"));
+        writer.write(outputString);
+
+        writer.close();
+    }
+
 }
