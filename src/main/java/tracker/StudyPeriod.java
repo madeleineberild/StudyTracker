8f456407aaea1be5b4983b091d978be8fb4bfa76
madeleineberild <madeleine.berild@hotmail.com>
2021-01-26 16:02:08 +0100

    Implement basis in StudyPeriod

--- src/main/java/tracker/StudyPeriod.java
+++ src/main/java/tracker/StudyPeriod.java
@@ -1,4 +1,30 @@
 package tracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudyPeriod {
+    String name;
+    int weeks;
+    List<StudyEntry> recorded;
+
+    public StudyPeriod(String name, int weeks) {
+        this.name = name;
+        this.weeks = weeks;
+        this.recorded = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeeks() {
+        return weeks;
+    }
+
+    public void getRecorded() {
+        //TODO: hitta ett sätt där vi kan ge ut en struktur med alla entries
+        // (till statistics) som inte är mutable och kan fucka upp allt
+    }
+
 }
