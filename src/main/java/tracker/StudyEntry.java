f95cbb83ad3c288d6f23cf4eea08703dd5f96319
Felix Apell Skjutar <felix.apellskjutar@gmail.com>
2021-01-27 23:36:29 +0100

    Implement class StudyEntry

--- src/main/java/tracker/StudyEntry.java
+++ src/main/java/tracker/StudyEntry.java
@@ -1,8 +1,74 @@
 package tracker;
 
+import java.time.LocalTime;
+
 /**
  * A StudyEntry is created for each recorded study event
  * TODO: figure out how to represent the amount of time. int? double? hours and minutes via some time class?
  */
 public class StudyEntry {
+
+    private LocalTime time;
+    private String description;
+    private Subject subject;
+
+    public StudyEntry (LocalTime time, String description, Subject subject) {
+
+        this.time = time;
+        this.subject = subject;
+        this.description = description;
+    }
+
+
+    //useful methods
+    public LocalTime getTime() {
+        return this.time;
+    }
+
+    public Subject getSubject () {
+        return this.subject;
+    }
+
+    //Not sure about this implementation, but worth a try.
+    public boolean isOfSuject(Subject subject) {
+        return subject.getClass() == this.subject.getClass();
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void adDescription(String desc) {
+        this.description += " " + desc;
+    }
+
+    //Builder pattern, if needed.
+    public class StudyEntryBuidler {
+        private LocalTime time;
+        private String description;
+        private Subject subject;
+
+        public StudyEntryBuidler () {}
+
+        public StudyEntryBuidler time(LocalTime time) {
+            this.time = time;
+            return this;
+        }
+
+        public StudyEntryBuidler subject(Subject subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        public StudyEntryBuidler description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public StudyEntry build() {
+            return new StudyEntry(time, description, subject);
+        }
+    }
+
+
 }
