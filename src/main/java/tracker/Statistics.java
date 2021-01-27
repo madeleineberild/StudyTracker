4433dcca805e34e71ac925091aa3e16a494b265c
Felix Apell Skjutar <felix.apellskjutar@gmail.com>
2021-01-28 00:18:56 +0100

    Implement function in Satistics for getting sum of a List of studyEntries

--- src/main/java/tracker/Statistics.java
+++ src/main/java/tracker/Statistics.java
@@ -1,4 +1,26 @@
 package tracker;
 
+import java.time.LocalTime;
+import java.util.List;
+
 public class Statistics {
+
+
+    /**
+     *
+     * @param entryList
+     * @return LocalTime, as the sum time of all instances in entryList
+     */
+    public static LocalTime taskSum(List<StudyEntry> entryList) {
+
+        //Use TemporalQuery as lamdba. query()
+        //Use getHour(), getMinute() on LocalTime object to extract.
+
+        int hours = entryList.stream().mapToInt(entry -> entry.getTime().getHour()).sum();
+        int minutes = entryList.stream().mapToInt(entry -> entry.getTime().getMinute()).sum();
+
+        return LocalTime.of(hours, minutes);
+    }
+
+
 }
